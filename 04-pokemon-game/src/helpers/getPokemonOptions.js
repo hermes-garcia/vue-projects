7dd3287c2c38994df363a4a243a4bdb3b67efba8
Hermes <hermes.garcia@bounteous.com>
2024-01-09 20:05:54 -0600

    Project 04 - Who is that pokemon?

---
+++ 04-pokemon-game/src/helpers/getPokemonOptions.js
@@ -0,0 +1,33 @@
+import pokemonApi from '@/api/pokemonApi';
+
+const getPokemon = () => {
+    const pokemonArray = Array.from(Array(650));
+    return pokemonArray.map((_, index) => index + 1)
+}
+
+
+const getPokemonOptions = () => {
+    const mixedPokemon = getPokemon().sort(
+        () => Math.random() - 0.5
+    );
+    return getPokemonNames(mixedPokemon.splice(0, 4))
+}
+
+const getPokemonNames = async ([a, b, c, d] = []) => {
+
+    const [p1, p2, p3, p4] = await Promise.all([
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`)
+    ]);
+
+    return [
+        { name: p1.data.name, id: p1.data.id },
+        { name: p2.data.name, id: p2.data.id },
+        { name: p3.data.name, id: p3.data.id },
+        { name: p4.data.name, id: p4.data.id },
+    ];
+}
+
+export default getPokemonOptions;
--- 04-pokemon-game/src/pages/PokemonPage.vue
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -1,14 +1,85 @@
 <template>
-  <h1>Who is that Pokemon?</h1>
-  <PokemonPicture />
-  <PokemonOptions />
+
+  <div v-if="selectedPokemon">
+    <h1>Who is that Pokemon?</h1>
+
+    <PokemonPicture
+        :pokemonId="selectedPokemon.id"
+        :showPokemon="showPokemon"
+    />
+
+    <PokemonOptions
+        :pokemon="pokemonArr"
+        @selection="checkAnswer"
+    />
+
+    <div class="answer" v-if="showAnswer">
+      <h2 class="fade-in">{{message}}</h2>
+      <button @click="newGame">New Game</button>
+    </div>
+  </div>
+
+  <h1 v-else>Fetching pokemon...</h1>
 </template>
 
 <script>
 import PokemonPicture from '@/components/PokemonPicture';
 import PokemonOptions from '@/components/PokemonOptions';
+import getPokemonOptions from '@/helpers/getPokemonOptions';
 
 export default {
-  components: {PokemonOptions, PokemonPicture}
+  components: {PokemonOptions, PokemonPicture},
+  data() {
+    return {
+      pokemonArr: [],
+      selectedPokemon: null,
+      showPokemon: false,
+      showAnswer: false,
+      message: '',
+    }
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions();
+      const rndInt = Math.floor(Math.random() * 4);
+      this.selectedPokemon = this.pokemonArr[rndInt];
+    },
+    checkAnswer(selectedId) {
+      this.showPokemon = true;
+      this.showAnswer = true;
+
+      if (selectedId === this.selectedPokemon.id) {
+        this.message = `Correct! It's ${this.selectedPokemon.name}`
+      } else {
+        this.message = `Oops! It was ${this.selectedPokemon.name}`
+      }
+    },
+    newGame() {
+      this.pokemonArr = [];
+      this.selectedPokemon = null;
+      this.showPokemon = false;
+      this.showAnswer = false;
+      this.message = '';
+      this.mixPokemonArray();
+    }
+  },
+  mounted() {
+    this.mixPokemonArray();
+  }
 }
 </script>
+
+<style scoped>
+.answer {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  h2 {
+  }
+
+  button {
+    margin: 0 auto;
+  }
+}
+</style>
