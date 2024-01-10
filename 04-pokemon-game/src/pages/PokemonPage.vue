49390e0a6c31b8dd47f2219e817b8d685d57dc25
Hermes <hermes.garcia@bounteous.com>
2024-01-09 18:57:01 -0600

    Project 04 scafolding

---
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Who is that Pokemon?</h1>
+  <PokemonPicture />
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture';
+import PokemonOptions from '@/components/PokemonOptions';
+
+export default {
+  components: {PokemonOptions, PokemonPicture}
+}
+</script>
